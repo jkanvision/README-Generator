b0ca2ae301a6599674dcab180ee4d37e05a9435a
JK code <112551853+jkanvision@users.noreply.github.com>
2022-10-25 01:07:30 -0400

    adds template.md and functions in generateMarkdown.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-// TODO: Include packages needed for this application
-const inquirer = requirer("inquirer");
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+// Creates an array of questions for user input
+const answers = [];
 
 inquirer
   .prompt([
     {
         type: "input",
         message: "What is the name of your project?",
-        name: "project-title",
+        name: "title",
     },
     {
         type: "input",
@@ -19,45 +22,57 @@ inquirer
     },
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "What command should be run to install dependencies?",
+        name: "installation",
     },
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "What does the user need to know about using this repo?",
+        name: "usage",
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "BSD 3", "GPL 3.0", "none"],
     },
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "What does the user need to know about contributing to this repo?",
+        name: "contribution",
     },
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "What command should be entered to run tests?",
+        name: "test",
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "Please provide an email address for questions about this project.",
+        name: "email",
     },
 
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+    const newReadMe = generateMarkdown(answers);
+   
+    fs.writeFile("README.md", newReadMe, (err) => err ? console.log(err) : console.log("Generating README..."));
   });
+  
 
 
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,91 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Creates a function that returns a license badge
+// If there is no license, returns an empty string
+function renderLicenseBadge() {
+  switch (answers) {
+    case license==="MIT":
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+    case "APACHE 2.0":
+      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case "BSD 3":
+      return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+      break;
+    case "GPL 3.0":
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+      break;
+    case "none":
+      return "";
+      break;
+  // ![License Badge](https://img.shields.io/badge/license-${data.license}-success?style=plastic)
+  };
+    
+};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Creates a function that returns the license link
+// If there is no license, returns an empty string
+function renderLicenseLink() {
+  if (license === "none") {
+    return "";
+  } else {
+    return `- [License](#license)`;
+  }
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Creates a function that returns the license section of README
+// If there is no license, returns an empty string
+function renderLicenseSection() {
+  if (license === "none") {
+    return "";
+    } else {
+      return `## License:
+      This project is licensed under the ${license} license.
+      <br>
+    
+    `;
+  }
+};
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}
+// Creates a function to generate markdown for README
+function generateMarkdown({title, description, installation, usage, license, contribution, test, github, email}) {
+  renderLicenseBadge();
+  return `# ${title}
+  <br>
+
+  ## Description: 
+  ${description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage);
+  ${renderLicenseLink()}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  <br>
+
+  ## Installation:
+  To install necessary dependencies, run the following command:
+  > ${installation}
+  <br>
+
+  ## Usage:
+  ${usage}
+  <br>
+
+  ${renderLicenseSection()}
+  ## Contributing:
+  ${contribution}
+  <br>
+
+  ## Tests:
+  To run tests, run the following command:
+  > ${test}
+  <br>
+
+  ## Questions:
+  If you have questions about this project, please contact me at ${email}. You can view my other projects by visiting my GitHub profile: ${github}.`;
+};
 
 module.exports = generateMarkdown;
