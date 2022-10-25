aa835a7ac37b55d68bfa86470a366a556a373852
JK code <112551853+jkanvision@users.noreply.github.com>
2022-10-25 16:51:10 -0400

    fixes functions in generateMarkdown file

--- newREADME.md
+++ newREADME.md
@@ -1,42 +1,60 @@
-# 
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  
+  # The XYZ Initiative
   <br>
 
-  undefined;
+  
 
   ## Description: 
-  
+  We intend to terminate the alphabet. All hail XYZ!  ;P
+  &nbsp;
+  &nbsp;
 
   ## Table of Contents
   - [Installation](#installation)
-  - [Usage](#usage);
+  - [Usage](#usage)
   - [License](#license)
   - [Contributing](#contributing)
   - [Tests](#tests)
   - [Questions](#questions)
   <br>
 
+  &nbsp;
+  &nbsp;
   ## Installation:
   To install necessary dependencies, run the following command:
-  > 
-  <br>
-
-  ## Usage:
+  > npm install
   
-  <br>
+
+  &nbsp;
+  &nbsp;
+  ## Usage:
+  This program is run through the command-line interface. You may need to install node.js to run it. 
+  &nbsp;
+  &nbsp;
+
 
   ## License:
-      This project is licensed under the undefined license.
-      <br>
-    
-    
-  ## Contributing:
-  
-  <br>
+      This project is licensed under the APACHE 2.0 license.
 
+    
+  &nbsp;
+  &nbsp;
+
+  ## Contributing:
+  Please submit a pull request
+  &nbsp;
+  &nbsp;
+
+  &nbsp;
+  &nbsp;
   ## Tests:
   To run tests, run the following command:
-  > 
-  <br>
-
+  > npm run test
+  &nbsp;
+  &nbsp;
+  
+  &nbsp;
+  &nbsp;
   ## Questions:
-  If you have questions about this project, please contact me at . You can view my other projects by visiting my GitHub profile: .
+  If you have questions about this project, please contact me at [jkanvision@knights.ucf.edu](mailto:jkanvision@knights.ucf.edu). You can view my other projects by visiting my GitHub profile: [https://github.com/jkanvision](https://github.com/jkanvision).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,69 +1,28 @@
-// Creates a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.project}
-  <br>
-
-  ${renderBadge()};
-
-  ## Description: 
-  ${data.description}
-
-  ## Table of Contents
-  - [Installation](#installation)
-  - [Usage](#usage)
-  ${renderLicenseLink()}
-  - [Contributing](#contributing)
-  - [Tests](#tests)
-  - [Questions](#questions)
-  <br>
-
-  ## Installation:
-  To install necessary dependencies, run the following command:
-  > ${data.installation}
-  <br>
-
-  ## Usage:
-  ${data.usage}
-  <br>
-
-  ${renderLicenseSection()}
-  ## Contributing:
-  ${data.contribution}
-  <br>
-
-  ## Tests:
-  To run tests, run the following command:
-  > ${data.test}
-  <br>
-
-  ## Questions:
-  If you have questions about this project, please contact me at ${data.email}. You can view my other projects by visiting my GitHub profile: ${data.github}.`;
-};
-
 // Creates a function that returns a license badge
 // If there is no license, returns an empty string
 renderBadge = function(answers) {
-  switch(answers) {
+  switch(answers.license) {
     case "MIT":
       response = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
       return response;
-      break;
-    case "APACHE":
-      response `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+      
+    case "APACHE 2.0":
+      response = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
       return response;
-      break;
-    case "BSD":
-      response `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+      
+    case "BSD 3":
+      response = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
       return response;
-      break;
-    case "GPL":
-      response `[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+      
+    case "GPL 3.0":
+      response
+      = `[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
       return response;
-      break;
+      
     case "none":
       response = ""
       return response;
-      break;
+      
    };
     
 };
@@ -71,8 +30,9 @@ renderBadge = function(answers) {
 // Creates a function that returns the license link
 // If there is no license, returns an empty string
 function renderLicenseLink(answers) {
-  if (answers === "none") {
-    return "";
+  if (answers.license === "none") {
+    response = ""
+    return response;
   } else {
     return `- [License](#license)`;
   }
@@ -81,15 +41,80 @@ function renderLicenseLink(answers) {
 // Creates a function that returns the license section of README
 // If there is no license, returns an empty string
 function renderLicenseSection(answers) {
-  if (answers === "none") {
-    return "";
+  if (answers.license === "none") {
+    response = ""
+    return response;
     } else {
       return `## License:
-      This project is licensed under the ${answers} license.
-      <br>
-    
+      This project is licensed under the ${answers.license} license.
+
     `;
   }
 };
+
+// Creates a function to generate markdown for README
+function generateMarkdown(data) {
+  return `${renderBadge(data)}
+  
+  # ${data.project}
+  <br>
+
+  
+
+  ## Description: 
+  ${data.description}
+  &nbsp;
+  &nbsp;
+
+  &nbsp;
+  &nbsp;
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  ${renderLicenseLink(data)}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  <br>
+
+  &nbsp;
+  &nbsp;
+  ## Installation:
+  To install necessary dependencies, run the following command:
+  > ${data.installation}
+  
+
+  &nbsp;
+  &nbsp;
+  ## Usage:
+  ${data.usage}
+  &nbsp;
+  &nbsp;
+
+  &nbsp;
+  &nbsp;
+  ${renderLicenseSection(data)}
+  &nbsp;
+  &nbsp;
+
+  ## Contributing:
+  ${data.contribution}
+  &nbsp;
+  &nbsp;
+
+  &nbsp;
+  &nbsp;
+  ## Tests:
+  To run tests, run the following command:
+  > ${data.test}
+  &nbsp;
+  &nbsp;
+  
+  &nbsp;
+  &nbsp;
+  ## Questions:
+  If you have questions about this project, please contact me at [${data.email}](mailto:${data.email}). You can view my other projects by visiting my GitHub profile: [${data.github}](${data.github}).`;
+};
+
 
 module.exports = generateMarkdown;
